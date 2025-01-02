35be476b0836205c56e371e65aa41ee168235cb0
TeoJT <teojohntaylor@gmail.com>
2025-01-02 16:39:20 +0000

    new blog post after a year

---
+++ docs/_site/assets/sketches/thumbnails.js
@@ -0,0 +1,13 @@
+let timeway_logo;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    frameRate(30);
+    
+    timeway_logo = loadImage('assets/laDefense.jpg');
+
+}
+  
+  function draw() {
+    background(40);
+  }
